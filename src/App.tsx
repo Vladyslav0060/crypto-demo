4ac952da71485dc01baa89d21091b171cbeec7ff
Vladyslav0060 <vlad.bortnik1@gmail.com>
2022-08-22 00:13:15 +0300

    Added cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { Container } from "@mui/system";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import CardItem from "./components/Card";
+import { Card, Grid } from "@mui/material";
+import AppBar from "./components/AppBar";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <AppBar />
+      <Container sx={{ marginY: 5 }}>
+        <Grid container spacing={5}>
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+        </Grid>
+      </Container>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<></>} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
